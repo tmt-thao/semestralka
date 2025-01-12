33f83b7c6a36e5134d4a412ed30df77acd0bb255
tmt-thao <vika.tran.minh@gmail.com>
2025-01-12 11:26:50 +0100

    klient rozlcelneny

--- client.c
+++ client.c
@@ -26,59 +26,6 @@ typedef struct {
     pthread_t render_tid;
 } ThreadArgs;
 
-void render_world(GameState *state) {
-    char world[MAX_WORLD_HEIGHT][MAX_WORLD_WIDTH];
-
-    for (int y = 0; y < state->height; y++) {
-        for (int x = 0; x < state->width; x++) {
-            world[y][x] = '.';
-        }
-    }
-
-    world[state->fruit.y][state->fruit.x] = 'F';
-
-    for (int i = 0; i < state->snake_length; i++) {
-        Position part = state->snake[i];
-        world[part.y][part.x] = (i == 0) ? 'H' : 'o';  // Hlava: 'H', telo: 'o'
-    }
-
-    for (int i = 0; i < state->num_obstacles; i++) {
-        Position obstacle = state->obstacles[i];
-        world[obstacle.y][obstacle.x] = '#';
-    }
-
-    printf("\033[H\033[J"); 
-    for (int y = 0; y < state->height; y++) {
-        for (int x = 0; x < state->width; x++) {
-            switch (world[y][x]) {
-                case 'H': 
-                    printf("\033[93mH\033[0m ");
-                    break;
-                case 'o':  
-                    printf("\033[92mo\033[0m ");
-                    break;
-                case 'F':  
-                    printf("\033[38;5;205mF\033[0m ");
-                    break;
-                case '#':  
-                    printf("\033[38;5;51m#\033[0m ");
-                    break;
-                default:  
-                    printf(". ");
-                    break;
-            }
-        }
-        printf("\n");
-    }
-
-    printf("Skóre: %d\n", state->score);
-    printf("Čas od začiatku hry: %d sekúnd\n", state->elapsed_time);
-    if (state->timed_mode) {
-        printf("Zostávajúci čas: %d sekúnd\n", state->time_limit - state->elapsed_time);
-    }
-}
-
-
 void show_menu(GameState *state) {
     printf("\n=== Nastavenie hry ===\n");
     printf("Zadajte šírku herného sveta: ");
@@ -207,6 +154,58 @@ void *input_thread(void *arg) {
     return NULL;
 }
 
+void render_world(GameState *state) {
+    char world[MAX_WORLD_HEIGHT][MAX_WORLD_WIDTH];
+
+    for (int y = 0; y < state->height; y++) {
+        for (int x = 0; x < state->width; x++) {
+            world[y][x] = '.';
+        }
+    }
+
+    world[state->fruit.y][state->fruit.x] = 'F';
+
+    for (int i = 0; i < state->snake_length; i++) {
+        Position part = state->snake[i];
+        world[part.y][part.x] = (i == 0) ? 'H' : 'o';  // Hlava: 'H', telo: 'o'
+    }
+
+    for (int i = 0; i < state->num_obstacles; i++) {
+        Position obstacle = state->obstacles[i];
+        world[obstacle.y][obstacle.x] = '#';
+    }
+
+    printf("\033[H\033[J"); 
+    for (int y = 0; y < state->height; y++) {
+        for (int x = 0; x < state->width; x++) {
+            switch (world[y][x]) {
+                case 'H': 
+                    printf("\033[93mH\033[0m ");
+                    break;
+                case 'o':  
+                    printf("\033[92mo\033[0m ");
+                    break;
+                case 'F':  
+                    printf("\033[38;5;205mF\033[0m ");
+                    break;
+                case '#':  
+                    printf("\033[38;5;51m#\033[0m ");
+                    break;
+                default:  
+                    printf(". ");
+                    break;
+            }
+        }
+        printf("\n");
+    }
+
+    printf("Skóre: %d\n", state->score);
+    printf("Čas od začiatku hry: %d sekúnd\n", state->elapsed_time);
+    if (state->timed_mode) {
+        printf("Zostávajúci čas: %d sekúnd\n", state->time_limit - state->elapsed_time);
+    }
+}
+
 void *render_thread(void *arg) {
     ThreadArgs *args = (ThreadArgs *)arg;
     SharedData *shared_data = args->shared_data;
@@ -226,8 +225,7 @@ void *render_thread(void *arg) {
     return NULL;
 }
 
-
-int main() {
+void start_server() {
     pid_t pid = fork();
 
     if (pid == -1) {
@@ -248,55 +246,70 @@ int main() {
     sem_wait(sem_init_done);
     sem_close(sem_init_done);
     sem_unlink("/sem_init_done");
+}
 
-
-    int shmid;
-    SharedData *shared_data;
-    sem_t *sem_server_ready, *sem_client_ready;
-
-    shmid = shmget(SHM_KEY, sizeof(SharedData), 0666);
+void connect_to_resources(SharedData **shared_data, sem_t **sem_server_ready, sem_t **sem_client_ready) {
+    int shmid = shmget(SHM_KEY, sizeof(SharedData), 0666);
     if (shmid == -1) {
         perror("Chyba pri pripojení k zdieľanej pamäti");
         exit(1);
     }
 
-    shared_data = (SharedData *)shmat(shmid, NULL, 0);
-    if (shared_data == (void *)-1) {
+    *shared_data = (SharedData *)shmat(shmid, NULL, 0);
+    if (*shared_data == (void *)-1) {
         perror("Chyba pri pripájaní k zdieľanej pamäti");
         exit(1);
     }
 
-    sem_server_ready = sem_open(SEM_SERVER_READY, 0);
-    sem_client_ready = sem_open(SEM_CLIENT_READY, 0);
-    if (sem_server_ready == SEM_FAILED || sem_client_ready == SEM_FAILED) {
+    *sem_server_ready = sem_open(SEM_SERVER_READY, 0);
+    *sem_client_ready = sem_open(SEM_CLIENT_READY, 0);
+    if (*sem_server_ready == SEM_FAILED || *sem_client_ready == SEM_FAILED) {
         perror("Chyba pri pripájaní k semaforom");
         exit(1);
     }
+}
+
+void initialize_threads(ThreadArgs *args) {
+    pthread_create(&args->input_tid, NULL, input_thread, args);
+    pthread_create(&args->render_tid, NULL, render_thread, args);
+}
+
+void run_client(ThreadArgs *args) {
+    while (args->running) {
+        pthread_mutex_lock(&args->shared_data->game_mutex);
+        if (args->shared_data->state.game_over_flag) {
+            pthread_mutex_unlock(&args->shared_data->game_mutex);
+            break;
+        }
+        pthread_mutex_unlock(&args->shared_data->game_mutex);
+        usleep(100000);
+    }
+}
+
+int main() {
+    SharedData *shared_data;
+    sem_t *sem_server_ready, *sem_client_ready;
+
+    start_server();
+    connect_to_resources(&shared_data, &sem_server_ready, &sem_client_ready);
+
 
     show_menu(&shared_data->state);
-
     pthread_mutex_lock(&shared_data->game_mutex);
     sem_post(sem_client_ready);
     pthread_mutex_unlock(&shared_data->game_mutex);
 
-    ThreadArgs args;
-    args.shared_data = shared_data;
-    args.sem_client_ready = sem_client_ready;
-    args.sem_server_ready = sem_server_ready;
-    args.running = 1;
+    ThreadArgs args = {
+        .shared_data = shared_data,
+        .sem_client_ready = sem_client_ready,
+        .sem_server_ready = sem_server_ready,
+        .running = 1
+    };
 
-    pthread_create(&args.input_tid, NULL, input_thread, &args);
-    pthread_create(&args.render_tid, NULL, render_thread, &args);
 
-    while (args.running) {
-        pthread_mutex_lock(&shared_data->game_mutex);
-        if (shared_data->state.game_over_flag) {
-            pthread_mutex_unlock(&shared_data->game_mutex);
-            break;
-        }
-        pthread_mutex_unlock(&shared_data->game_mutex);
-        usleep(100000);
-    }
+    initialize_threads(&args);
+    run_client(&args);
+
 
     pthread_cancel(args.input_tid);
     pthread_join(args.input_tid, NULL);
@@ -304,8 +317,12 @@ int main() {
     pthread_cancel(args.render_tid);
     pthread_join(args.render_tid, NULL);
 
+
     sem_close(sem_server_ready);
     sem_close(sem_client_ready);
     shmdt(shared_data);
+
+    printf("Klient: Korektne ukončený.\n");
     return 0;
 }
+
